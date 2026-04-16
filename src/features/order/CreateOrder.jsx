e86a1750abad04bb85fecf46f38b2e817fcb61f4
Ahmed Sherif <ahmedsherifeg22@gmail.com>
2026-04-16 22:45:10 +0200

    feat: handle async address fetching with extraReducers

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,13 +1,15 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
+
+import store from "./../../store";
+import { createOrder } from "../../services/apiRestaurant";
+import { clearCart, getCart, getTotalCartPrice } from "../cart/CartSlice";
+import { fetchAddress } from "../user/userSlice";
+import { formatCurrency } from "./../../utils/helpers";
 
 import Button from "../../UI/Button";
 import EmptyCart from "./../cart/EmptyCart";
-import { createOrder } from "../../services/apiRestaurant";
-import { useSelector } from "react-redux";
-import { clearCart, getCart, getTotalCartPrice } from "../cart/CartSlice";
-import store from "./../../store";
-import { formatWithCursor } from "prettier";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -17,14 +19,22 @@ const isValidPhone = (str) =>
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
-  const userName = useSelector((state) => state.user.userName);
   const totalCartPrice = useSelector(getTotalCartPrice);
   const cart = useSelector(getCart);
+  const {
+    userName,
+    status: addressStatus,
+    address,
+    position,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
 
+  const dispatch = useDispatch();
   const navigation = useNavigation();
   const formErrors = useActionData();
   const isSubmitting = navigation.state === "submitting";
 
+  const isLoadingAddress = addressStatus === "loading";
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + priorityPrice;
   if (!cart.length) return <EmptyCart />;
@@ -57,7 +67,7 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
@@ -65,10 +75,28 @@ function CreateOrder() {
               type="text"
               name="address"
               required
+              disabled={isLoadingAddress}
+              defaultValue={address}
             />
           </div>
+          <span className="absolute right-1.25 z-50">
+            <Button
+              type="small"
+              disabled={isLoadingAddress}
+              onClick={(e) => {
+                e.preventDefault();
+                dispatch(fetchAddress());
+              }}
+            >
+              Get position
+            </Button>
+          </span>
         </div>
-
+        {addressStatus === "error" && (
+          <p className="mt-2 mb-5 rounded-lg bg-red-100 p-2 text-xs text-red-700">
+            {errorAddress}
+          </p>
+        )}
         <div className="mb-12 flex items-center gap-5">
           <input
             className="h-4 w-4 accent-amber-400 transition-all focus:ring focus:ring-amber-400 focus:ring-offset-2 focus:outline-none"
@@ -84,12 +112,21 @@ function CreateOrder() {
         </div>
 
         <div>
-          {/* hidden input to add cart data into the Form */}
+          {/* hidden input to add cart data into and Address info the Form */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type="primary">
+          <input
+            type="hidden"
+            name="address"
+            value={
+              position.latitude
+                ? `${position.latitude} , ${position.longitude}`
+                : ""
+            }
+          />
+          <Button disabled={isSubmitting || isLoadingAddress} type="primary">
             {isSubmitting
               ? "Placing Order..."
-              : `Order now from ${formatWithCursor(totalPrice)}`}
+              : `Order now from ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
       </Form>
